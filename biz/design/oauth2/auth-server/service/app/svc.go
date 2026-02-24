f35935be1987fd88f635d8d5a73dfec4f4a0124b
LeeSeika <leeseika@outlook.com>
2026-02-24 17:26:21 +0800

    解释 create auth session

--- biz/design/oauth2/auth-server/service/app/svc.go
+++ biz/design/oauth2/auth-server/service/app/svc.go
@@ -16,6 +16,9 @@ var _initAppOnce sync.Once
 type (
 	App interface {
 		List(ctx context.Context) ([]*object.App, error)
+		// CreateAuthSession 是在授权过程中创建一个临时的授权会话（由 js 在渲染安装页面时自动调用），保证进入 auth 页面和点击授权按钮的是同一个用户，防止 CSRF 攻击
+		// 这并不是一个标准的 OAuth2 流程，而是我们为了安全性在授权流程中添加的一个额外步骤
+		// 在点击安装后，发请求到 oauth/app/authorize 的时候，就会校验这个 token
 		CreateAuthSession(ctx context.Context, authInfo *jsonmodel.AuthInfo, payload *jsonmodel.OAuthPayload) (string, error)
 	}
 
